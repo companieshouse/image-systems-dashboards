953b8752162448ce256d573340826c375ad14710
Marc Ransome <mransome@companieshouse.gov.uk>
2025-02-20 10:03:30 +0000

    Remove unused variable

--- groups/dashboards/variables.tf
+++ groups/dashboards/variables.tf
@@ -7,14 +7,3 @@ variable "region" {
   type        = string
   description = "The AWS region in which resources will be created."
 }
-
-variable "tuxedo_service_subtypes" {
-  type        = list(string)
-  description = "The service subtype name to be used when creating AWS resources."
-  default = [
-    "frontend",
-    "ois",
-    "fil",
-    "ais"
-  ]
-}
